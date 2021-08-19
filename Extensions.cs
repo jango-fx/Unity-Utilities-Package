482f5cfdf1c37fe87cf9c4689fbd05a6c55fecfd
jango-fx <felix.groll@gmail.com>
2021-08-19 10:27:58 +0200

    added extension methods
    - added ToArrayString() method, to create a string of all array items

---
+++ Extensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// using ExtensionMethods;
+
+namespace ƒx.UnityUtils
+{
+    public static class ListExtensions
+    {
+        public static string ToArrayString<T>(this List<T> values)
+        {
+            string format = "";
+            for (int i = 1; i <= values.Count; i *= 10)
+                format += "0";
+            
+            string msg = "";
+            for (int i = 0; i < values.Count; i++)
+            {
+                msg += "\n  [" + i.ToString(format) + "]: " + values[i].ToString() + "";
+            }
+            return msg;
+        }
+    }
+}
